a8b4be79d697ca2a1251b06c1c8188af56e55803
Eduard Maximovich <edward.vstock@gmail.com>
2017-12-27 01:44:46 +0300

    doxygen bin verify

--- doc_gen.sh
+++ doc_gen.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+DOXY_BIN=`which doxygen | tr '\n' ' '`
+if (( DOXY_BIN == "" ));
+then
+	echo "Doxygen binary did not found!"
+	exit 1
+fi
+
 cd src
 ln -s ../README.md README.md
 cd ../docs
