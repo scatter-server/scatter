41069951225129c99027732f35ceea715e6e07f8
Eduard Maximovich <edward.vstock@gmail.com>
2017-12-27 00:18:04 +0300

    travis, build script

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 git clone --recursive https://github.com/edwardstock/toolboxpp.git toolbox
 mkdir -p toolbox/build
 cd toolbox/build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+sudo cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --target toolboxpp
-cmake --build . --target install
+sudo cmake --build . --target install
 cd ../../
 
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+sudo cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --target wsserver
-cmake --build . --target install
+sudo cmake --build . --target install
