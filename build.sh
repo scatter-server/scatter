6a7ce196fa3597de8559d4331396f5002d757a44
Eduard Maximovich <edward.vstock@gmail.com>
2017-12-26 18:55:04 +0300

    exclusing coverage and tests from deps

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 git clone --recursive https://github.com/edwardstock/toolboxpp.git toolbox
 mkdir -p toolbox/build
 cd toolbox/build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
-cmake --build .
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --target toolboxpp
 cmake --build . --target install
 cd ../../
 
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_TEST=ON
-cmake --build .
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_TEST=OFF
+cmake --build . --target wsserver
 cmake --build . --target install
