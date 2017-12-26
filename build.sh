ce0a8f305c24336164643f3bcbbe72cc83f7f201
Eduard Maximovich <edward.vstock@gmail.com>
2017-12-27 00:33:31 +0300

    check cmake version before compile

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+CMAKE_BIN=`which cmake | tr '\n' ' '`
+CMAKE_VERS_MAJOR=`${CMAKE_BIN} --version | grep version | awk '{print $3}' | awk -F '.' '{print $1}'`
+
+if [ "${CMAKE_VERS_MAJOR}" -lt 3 ]
+then
+	echo "CMake version must be >= 3.0.0"
+	exit 1
+fi
 
 git clone --recursive https://github.com/edwardstock/toolboxpp.git toolbox
 mkdir -p toolbox/build
 cd toolbox/build
-sudo cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --target toolboxpp
-sudo cmake --build . --target install
+sudo ${CMAKE_BIN} .. -DCMAKE_BUILD_TYPE=Release
+${CMAKE_BIN} --build . --target toolboxpp
+sudo ${CMAKE_BIN} --build . --target install
 cd ../../
 
 mkdir -p build && cd build
-sudo cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --target wsserver
-sudo cmake --build . --target install
+sudo ${CMAKE_BIN} .. -DCMAKE_BUILD_TYPE=Release
+${CMAKE_BIN} --build . --target wsserver
+sudo ${CMAKE_BIN} --build . --target install
