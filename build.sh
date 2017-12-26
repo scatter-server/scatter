aaf80fbefc862b7d6d2d518510ce27fdfd3bd199
Eduard Maximovich <edward.vstock@gmail.com>
2017-12-27 01:01:47 +0300

    searching correct cmake if installed but not found in current PATH

--- build.sh
+++ build.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
-CMAKE_BIN=`which cmake | tr '\n' ' '`
-CMAKE_VERS_MAJOR=`${CMAKE_BIN} --version | grep version | awk '{print $3}' | awk -F '.' '{print $1}'`
+function findCmake {
+	local res=`find /usr/local/ -name "cmake" | grep "/bin" | sort | head -1`
+	echo "${res}"
+}
 
-if [ "${CMAKE_VERS_MAJOR}" -lt 3 ]
+function checkCmakeVers {
+	local version_major=`$1 --version | grep version | awk '{print $3}' | awk -F '.' '{print $1}'`
+	if (( version_major < 3 )); then
+	    echo "0"
+	else
+		echo "1"
+	fi
+}
+
+# trying to use default version first
+CMAKE_BIN=`which cmake | tr '\n' ' '`
+CMAKE_VALID=`checkCmakeVers ${CMAKE_BIN}`
+
+
+if [ "${CMAKE_VALID}" -eq 0 ]
 then
-	echo "CMake version must be >= 3.0.0"
-	exit 1
+	echo "CMake version must be >= 3.0.0. Trying to search in /usr/local"
+	CMAKE_BIN=`findCmake`
+	CMAKE_VALID=`checkCmakeVers ${CMAKE_BIN}`
+	if [ "${CMAKE_BIN}" == "" ] || [ "${CMAKE_VALID}" -eq 0 ]
+	then
+		echo "Didn't found correct version of CMake. Exiting..."
+		exit 1
+	else
+		echo "Found correct cmake: ${CMAKE_BIN}"
+	fi
 fi
 
 git clone --recursive https://github.com/edwardstock/toolboxpp.git toolbox
