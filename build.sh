d16e3c938aab0af0f7d086c97e8042565015a4a8
Eduard Maximovich <edward.vstock@gmail.com>
2017-12-27 17:30:48 +0300

    backquotes to $(...)

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 function findCmake {
-	local res=`find /usr/local/ -name "cmake" | grep "/bin" | sort | head -1`
+	local res=$(find /usr/local/ -name "cmake" | grep "/bin" | sort | head -1)
 	echo "${res}"
 }
 
 function checkCmakeVers {
-	local version_major=`$1 --version | grep version | awk '{print $3}' | awk -F '.' '{print $1}'`
+	if [ "$1" == "" ]
+	then
+		echo "0"
+		return
+	fi
+
+	local version_major=$($1 --version | grep version | awk '{print $3}' | awk -F '.' '{print $1}')
 	if (( version_major < 3 )); then
 	    echo "0"
 	else
@@ -15,15 +21,14 @@ function checkCmakeVers {
 }
 
 # trying to use default version first
-CMAKE_BIN=`which cmake | tr '\n' ' '`
-CMAKE_VALID=`checkCmakeVers ${CMAKE_BIN}`
-
+CMAKE_BIN=$(which cmake | tr '\n' ' ')
+CMAKE_VALID=$(checkCmakeVers ${CMAKE_BIN})
 
 if [ "${CMAKE_VALID}" -eq 0 ]
 then
 	echo "CMake version must be >= 3.0.0. Trying to search in /usr/local"
-	CMAKE_BIN=`findCmake`
-	CMAKE_VALID=`checkCmakeVers ${CMAKE_BIN}`
+	CMAKE_BIN=$(findCmake)
+	CMAKE_VALID=$(checkCmakeVers ${CMAKE_BIN})
 	if [ "${CMAKE_BIN}" == "" ] || [ "${CMAKE_VALID}" -eq 0 ]
 	then
 		echo "Didn't found correct version of CMake. Exiting..."
